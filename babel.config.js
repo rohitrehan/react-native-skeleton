2a29f6c1b7d3e7c3676c09dee81357e60f19ff59
sg <sg@iboxindia.com>
2024-07-21 20:06:42 +0530

    cleanup

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
-};
--- src/config/AppConfigProvider.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {ApplicationConfig, defaultConfig} from './DefaultConfig';
-
-const ConfigContext: React.Context<ApplicationConfig> =
-  React.createContext<ApplicationConfig>(defaultConfig);
-
-export default ConfigContext;
